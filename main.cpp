df853da9409b61ac4d5bbe8b866f79b169ea5509
Joel Lisanti <joel.c.lisanti@gmail.com>
2023-06-11 16:12:43 -0400

    basics

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include <fstream>
+
+struct Geom {
+    double length_combustor;
+    double radius_throat_1;
+    double radius_throat_2;
+    double radius_cone;
+}
+
+int main() 
+{
+    std::cerr << "**WARNING**" << std::endl;
+
+    return 0;
+}
